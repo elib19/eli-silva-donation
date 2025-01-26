67e42cb3a569600970f0148e2e25d43effce741a
elib19 <143918479+elib19@users.noreply.github.com>
2025-01-26 16:55:16 -0300

    Create reports.js

---
+++ assets/js/reports.js
@@ -0,0 +1,9 @@
+// JavaScript para o plugin de doações
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Exemplo de funcionalidade: exibir uma mensagem de sucesso após o envio do feedback
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('feedback') && urlParams.get('feedback') === 'sucesso') {
+        alert('Reportado com sucesso!');
+    }
+});
