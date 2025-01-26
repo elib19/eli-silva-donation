1a782433cef930d2be0978f6db90fb19752b222e
elib19 <143918479+elib19@users.noreply.github.com>
2025-01-26 17:12:33 -0300

    Delete assets/js/reports.js

--- assets/js/reports.js
+++
@@ -1,9 +0,0 @@
-// JavaScript para o plugin de doações
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Exemplo de funcionalidade: exibir uma mensagem de sucesso após o envio do feedback
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('feedback') && urlParams.get('feedback') === 'sucesso') {
-        alert('Reportado com sucesso!');
-    }
-});
